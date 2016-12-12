0884a65fa9fdf53a491fed0358d61424ff6e7202
Kevin Lin <kevinl1@andrew.cmu.edu>
2016-12-12 15:56:22 -0500

    ERROR: SOLVER IS WRONG. Switching to ADMM right now

--- gLatentModel/R/estimateA.R
+++ gLatentModel/R/estimateA.R
@@ -28,37 +28,37 @@ pure_nodes <- function(c_mat, k){
   order(abs(diag(c_mat)), decreasing = T)[1:k]
 }
 
-# .optim_solver_constrainLS <- function(vec, mat, depth = 1, max_depth = 3,
-#   grid_length = 100, multiplier_range = 10, shift_idx = 1, start = -10,
-#   end = 10, tol = 1e-4, warnings = 0){
-#
-#   if(warnings > max_depth) stop("Error in solving for A. Cannot calibrate.")
-#
-#   gamma_grid <- seq(start, end, length.out = grid_length)
-#   inv_mat <- solve(t(mat)%*%mat)
-#
-#   u_mat <- sapply(gamma_grid, .KKT_grid_solver,
-#     vec = vec, mat = mat, inv_mat = inv_mat)
-#   u_sum <- apply(u_mat, 2, sum)
-#   idx <- which.min(abs(u_sum - 1))
-#
-#   if(max(u_sum) < 1-tol | min(u_sum) > 1+tol){
-#     #cannot find right u_sum
-#     dif <- multiplier_range*abs(end - start)
-#     .optim_solver_constrainLS(vec, mat, start = start-dif,
-#       end = end+dif, warnings = warnings+1)
-#   } else if(depth < max_depth){
-#     #go to next depth
-#     dif <- gamma_grid[2] - gamma_grid[1]
-#     next_start <- gamma_grid[max(1, idx - shift_idx)] - dif
-#     next_end <- gamma_grid[min(grid_length, idx + shift_idx)] + dif
-#     .optim_solver_constrainLS(vec, mat, start = next_start,
-#       end = next_end, depth = depth+1)
-#   } else{
-#     #return
-#     as.numeric(u_mat[,idx])
-#   }
-# }
+.optim_solver_constrainLS <- function(vec, mat, depth = 1, max_depth = 3,
+  grid_length = 100, multiplier_range = 10, shift_idx = 1, start = -10,
+  end = 10, tol = 1e-4, warnings = 0){
+
+  if(warnings > max_depth) stop("Error in solving for A. Cannot calibrate.")
+
+  gamma_grid <- seq(start, end, length.out = grid_length)
+  inv_mat <- solve(t(mat)%*%mat)
+
+  u_mat <- sapply(gamma_grid, .KKT_grid_solver,
+    vec = vec, mat = mat, inv_mat = inv_mat)
+  u_sum <- apply(u_mat, 2, sum)
+  idx <- which.min(abs(u_sum - 1))
+
+  if(max(u_sum) < 1-tol | min(u_sum) > 1+tol){
+    #cannot find right u_sum
+    dif <- multiplier_range*abs(end - start)
+    .optim_solver_constrainLS(vec, mat, start = start-dif,
+      end = end+dif, warnings = warnings+1)
+  } else if(depth < max_depth){
+    #go to next depth
+    dif <- gamma_grid[2] - gamma_grid[1]
+    next_start <- gamma_grid[max(1, idx - shift_idx)] - dif
+    next_end <- gamma_grid[min(grid_length, idx + shift_idx)] + dif
+    .optim_solver_constrainLS(vec, mat, start = next_start,
+      end = next_end, depth = depth+1)
+  } else{
+    #return
+    as.numeric(u_mat[,idx])
+  }
+}
 
 .KKT_grid_solver <- function(gamma, vec, mat, inv_mat){
   K <- ncol(mat)
--- gLatentModel/tests/testthat/test_estimateA.R
+++ gLatentModel/tests/testthat/test_estimateA.R
@@ -105,3 +105,89 @@ test_that(".KKT_grid_solver returns properly", {
 #     expect_true(all(lambda >= -1e-4))
 #   }
 # })
+
+##########################################
+
+## .optim_solver_constrainLS is correct
+
+test_that(".optim_solver_constrainLS returns properly", {
+  set.seed(10)
+  K <- 5
+  vec <- stats::rnorm(K)
+  mat <- matrix(stats::rnorm(K^2), K, K); mat <- mat + t(mat)
+
+  res <- .optim_solver_constrainLS(vec, mat)
+
+  expect_true(is.numeric(res))
+  expect_true(!is.matrix(res))
+  expect_true(length(res) == K)
+})
+
+# test_that(".optim_solver_constrainLS enforces primal feasibility",{
+#   for(trial in 1:50){
+#     set.seed(10*trial)
+#     K <- 5
+#     vec <- stats::rnorm(K)
+#     mat <- matrix(stats::rnorm(K^2), K, K); mat <- mat + t(mat)
+#
+#     res <- .optim_solver_constrainLS(vec, mat)
+#
+#     expect_true(all(res >= -1e-4))
+#     expect_true(abs(sum(res) - 1) <= 1e-4)
+#   }
+# })
+#
+# test_that(".optim_solver_contrainLS gives the same value of gamma", {
+#   for(trial in 1:50){
+#     set.seed(10*trial)
+#     K <- 5
+#     vec <- stats::rnorm(K)
+#     mat <- matrix(stats::rnorm(K^2), K, K); mat <- mat + t(mat)
+#
+#     res <- .optim_solver_constrainLS(vec, mat)
+#
+#     zz = -2*(vec-res%*%mat)%*%mat
+#     idx = which(res != 0)
+#     gamma = mean(-zz[idx])
+#
+#     expect_true(all(abs(-zz[idx] - gamma) <= 1e-4))
+#   }
+# })
+#
+# test_that(".optim_solver_constrainLS enforces dual feasibility", {
+#   for(trial in 1:50){
+#     set.seed(10*trial)
+#     K <- 5
+#     vec <- stats::rnorm(K)
+#     mat <- matrix(stats::rnorm(K^2), K, K); mat <- mat + t(mat)
+#
+#     res <- .optim_solver_constrainLS(vec, mat)
+#
+#     zz = -2*(vec-res%*%mat)%*%mat
+#     idx = which(res != 0)
+#     gamma = mean(-zz[idx])
+#     lambda = rep(0, K)
+#     lambda[-idx] = zz[-idx] + gamma
+#
+#     expect_true(all(lambda >= -1e-4))
+#   }
+# })
+#
+# test_that(".optim_solver_constrainLS enforces complimentary slackness", {
+#   for(trial in 1:50){
+#     set.seed(10*trial)
+#     K <- 5
+#     vec <- stats::rnorm(K)
+#     mat <- matrix(stats::rnorm(K^2), K, K); mat <- mat + t(mat)
+#
+#     res <- .optim_solver_constrainLS(vec, mat)
+#
+#     zz = -2*(vec-res%*%mat)%*%mat
+#     idx = which(res != 0)
+#     gamma = mean(-zz[idx])
+#     lambda = rep(0, K)
+#     lambda[-idx] = zz[-idx] + gamma
+#
+#     expect_true(all(lambda*res >= -1e-4))
+#   }
+# })
