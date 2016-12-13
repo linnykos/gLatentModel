5c0c705c902ed7d67e54c1d0bcba4f65d004d005
Kevin Lin <kevinl1@andrew.cmu.edu>
2016-12-13 02:59:19 -0500

    Added tests for reestimating theta

--- gLatentModel/R/reestimateTheta.R
+++ gLatentModel/R/reestimateTheta.R
@@ -1,6 +1,6 @@
 reestimate_theta <- function(cov_mat, gamma_mat, a_mat){
-  stopifnot(ncol(a_mat) == ncol(gamma_mat), ncol(gamma_mat) == nrow(gamma_mat))
-  stopifnot(nrow(a_mat) == nrow(cov_mat))
+  stopifnot(ncol(gamma_mat) == nrow(gamma_mat))
+  stopifnot(nrow(a_mat) == nrow(cov_mat), nrow(a_mat) == nrow(gamma_mat))
   stopifnot(all(as.numeric(a_mat) %in% c(0,1)))
 
   a_inv <- solve(t(a_mat) %*% a_mat)
---
+++ gLatentModel/tests/testthat/test_reestimateTheta.R
@@ -0,0 +1,18 @@
+context("Test reestimate theta")
+
+## reestimate_theta is correct
+
+test_that("reestimate_theta returns properly", {
+  set.seed(10)
+  dat <- MASS::mvrnorm(20, rep(0, 6), diag(6))
+  cov_mat <- stats::cov(dat)
+  gamma_mat <- diag(stats::rnorm(6))
+  a_mat <- rbind(diag(3), diag(3))
+
+  res <- reestimate_theta(cov_mat, gamma_mat, a_mat)
+
+  expect_true(is.matrix(res))
+  expect_true(is.numeric(res))
+  expect_true(sum(abs(res - t(res))) < 1e-4)
+  expect_true(all(dim(res) == c(3,3)))
+})
