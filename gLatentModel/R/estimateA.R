145fddbdca0c65cdb6cf1ca00f27a0372605ab03
Kevin Lin <kevinl1@andrew.cmu.edu>
2016-12-13 00:53:01 -0500

    Some form of it is working... (for ADMM)

--- gLatentModel/R/estimateA.R
+++ gLatentModel/R/estimateA.R
@@ -34,9 +34,9 @@ pure_nodes <- function(c_mat, k){
   iter <- 1
   beta_vec_prev <- rep(0, ncol(mat))
 
-  beta_mat <- matrix(0, ncol(mat), max_iter)
-  z_mat <- matrix(0, ncol(mat), max_iter)
-  lambda_mat <- matrix(0, ncol(mat), max_iter)
+  #beta_mat <- matrix(0, ncol(mat), max_iter)
+  #z_mat <- matrix(0, ncol(mat), max_iter)
+  #lambda_mat <- matrix(0, ncol(mat), max_iter)
 
   while(iter < max_iter){
     beta_vec_new <- .ADMM_solver_primal(vec, mat, lambda_vec, z_vec, rho)
@@ -47,24 +47,69 @@ pure_nodes <- function(c_mat, k){
     beta_vec_old <- beta_vec_new
     iter <- iter + 1
 
-    beta_mat[,iter] <- beta_vec_new
-    z_mat[,iter] <- z_vec
-    lambda_mat[,iter] <- lambda_vec
+    #beta_mat[,iter] <- beta_vec_new
+    #z_mat[,iter] <- z_vec
+    #lambda_mat[,iter] <- lambda_vec
   }
 
   #list(beta = beta_mat, z = z_mat, lambda = lambda_mat)
-  beta_vec_new
+  z_vec
 }
 
 .ADMM_solver_primal <- function(vec, mat, lambda_vec, z_vec, rho){
   K <- ncol(mat)
 
-  res <- solve(-2*t(mat)%*%mat + diag(K))%*%(-lambda_vec + rho * z_vec)
+  res <- solve(2*t(mat)%*%mat + rho*diag(K))%*%(-lambda_vec + rho * z_vec +
+      2*t(mat)%*%vec)
+  #res
   as.numeric(res/sum(res))
 }
 
-.ADMM_solver_dual <- function(beta_vec, rho, lambda_vec){
-  res <- 2/rho * (lambda_vec + rho*beta_vec/2)
-  res[res < 0] <- 0
-  as.numeric(res)
+.ADMM_solver_dual <- function(beta_vec, rho, lambda_vec, depth = 1,
+  max_depth = 3, grid_length = 100, start = -10, end = 10,
+  warnings = 0, tol = 1e-4, multiplier_range = 10){
+
+  if(warnings > max_depth) stop("Error in solving for A dual. Cannot calibrate.")
+
+  gamma_grid <- seq(start, end, length.out = grid_length)
+
+  dual_mat <- sapply(gamma_grid, function(x){
+    sapply(beta_vec + 1/rho*(lambda_vec + x), function(y){max(y,0)})
+  })
+  dual_sum <- apply(dual_mat, 2, sum)
+
+  res <- .select_minimum_index(dual_sum, gamma_grid)
+
+  if(max(dual_sum) < 1-tol | min(dual_sum) > 1+tol){
+    #cannot find right u_sum
+    dif <- multiplier_range*abs(end - start)
+    .ADMM_solver_dual(beta_vec, rho, lambda_vec, start = start-dif,
+      end = end+dif, warnings = warnings+1)
+  } else if(depth < max_depth & abs(dual_sum[res$idx] - 1) > tol){
+    #go to next depth
+    .ADMM_solver_dual(beta_vec, rho, lambda_vec, start = res$start,
+      end = res$end, depth = depth+1, warnings = 0)
+  } else{
+    #return
+    as.numeric(dual_mat[,res$idx])
+  }
+}
+
+.select_minimum_index <- function(vec, gamma_grid){
+  stopifnot(length(vec) == length(gamma_grid))
+
+  len <- length(gamma_grid)
+  idx <- which.min(abs(vec - 1))
+  val <- vec[idx]
+  dif <- gamma_grid[2] - gamma_grid[1]
+
+  uniq_vec <- sort(unique(vec))
+  uniq_idx <- which(uniq_vec == val)
+
+  tmp <- which(vec == uniq_vec[max(1, uniq_idx - 1)])
+  start <- gamma_grid[tmp[length(tmp)]] - dif
+  tmp <- which(vec == uniq_vec[min(len, uniq_idx + 1)])
+  end <- gamma_grid[tmp[1]] + dif
+
+  list(idx = idx, start = start, end = end)
 }
--- gLatentModel/tests/testthat/test_estimateA.R
+++ gLatentModel/tests/testthat/test_estimateA.R
@@ -51,10 +51,22 @@ test_that(".optim_solver_constrainLS returns properly", {
   expect_true(is.numeric(res))
   expect_true(!is.matrix(res))
   expect_true(length(res) == K)
+ })
+
+
+test_that(".optim_solver_constrainLS gets an obvious answer correct", {
+  set.seed(10)
+  vec <- c(1:5)
+  mat <- matrix(rnorm(25),5,5); mat <- mat + t(mat)
+  mat[,1] <- vec; mat[1,] <- vec
+
+  res <- .optim_solver_constrainLS(vec, mat)
+
+  expect_true(sum((vec - mat %*% res)^2) <= sum((vec - mat %*% c(1,rep(0,4)))^2))
 })
 
 test_that(".optim_solver_constrainLS enforces primal feasibility",{
-  for(trial in 1:50){
+  for(trial in 1:10){
     set.seed(10*trial)
     K <- 5
     vec <- stats::rnorm(K)
@@ -68,7 +80,7 @@ test_that(".optim_solver_constrainLS enforces primal feasibility",{
 })
 
 test_that(".optim_solver_contrainLS gives the same value of gamma", {
-  for(trial in 1:50){
+  for(trial in 1:10){
     set.seed(10*trial)
     K <- 5
     vec <- stats::rnorm(K)
@@ -85,7 +97,7 @@ test_that(".optim_solver_contrainLS gives the same value of gamma", {
 })
 
 test_that(".optim_solver_constrainLS enforces dual feasibility", {
-  for(trial in 1:50){
+  for(trial in 1:10){
     set.seed(10*trial)
     K <- 5
     vec <- stats::rnorm(K)
@@ -93,31 +105,31 @@ test_that(".optim_solver_constrainLS enforces dual feasibility", {
 
     res <- .optim_solver_constrainLS(vec, mat)
 
-    zz = -2*(vec-res%*%mat)%*%mat
+    zz = -2*t(mat)%*%(vec-mat%*%res)
     idx = which(res != 0)
-    gamma = mean(-zz[idx])
+    gamma = mean(zz[idx])
     lambda = rep(0, K)
-    lambda[-idx] = zz[-idx] + gamma
+    lambda[-idx] = zz[-idx] - gamma
 
     expect_true(all(lambda >= -1e-4))
   }
 })
-
-test_that(".optim_solver_constrainLS enforces complimentary slackness", {
-  for(trial in 1:50){
-    set.seed(10*trial)
-    K <- 5
-    vec <- stats::rnorm(K)
-    mat <- matrix(stats::rnorm(K^2), K, K); mat <- mat + t(mat)
-
-    res <- .optim_solver_constrainLS(vec, mat)
-
-    zz = -2*(vec-res%*%mat)%*%mat
-    idx = which(res != 0)
-    gamma = mean(-zz[idx])
-    lambda = rep(0, K)
-    lambda[-idx] = zz[-idx] + gamma
-
-    expect_true(all(lambda*res >= -1e-4))
-  }
-})
+#
+# test_that(".optim_solver_constrainLS enforces complimentary slackness", {
+#   for(trial in 1:10){
+#     set.seed(10*trial)
+#     K <- 5
+#     vec <- stats::rnorm(K)
+#     mat <- matrix(stats::rnorm(K^2), K, K); mat <- mat + t(mat)
+#
+#     res <- .optim_solver_constrainLS(vec, mat)
+#
+#     zz = -2*(vec-res%*%mat)%*%mat
+#     idx = which(res != 0)
+#     gamma = mean(-zz[idx])
+#     lambda = rep(0, K)
+#     lambda[-idx] = zz[-idx] + gamma
+#
+#     expect_true(all(lambda*res >= -1e-4))
+#   }
+# })
