2cee4b3189e269ca97fa92bf4fa360c786390c4c
klin1234@gmail.com <klin1234@gmail.com>
2017-02-13 15:32:33 -0500

    Added tests

--- gLatentModel/tests/testthat/test_gLatentModel.R
+++ gLatentModel/tests/testthat/test_gLatentModel.R
@@ -183,47 +183,6 @@ test_that("gLatentModel will output the right number of clusters", {
 
 ##########################
 
-## .adjustment_cluster is correct
-
-test_that(".adjustment_cluster correctly removes one cluster", {
-  clust <- rep(1:6, each = 5)
-  res <- .adjustment_cluster(clust, 5)
-
-  expect_true(all(sort(unique(res)) == c(1:5)))
-})
-
-test_that(".adjustment_cluster correctly adds one cluster", {
-  clust <- rep(1:6, each = 5)
-  res <- .adjustment_cluster(clust, 7)
-
-  expect_true(all(sort(unique(res)) == c(1:7)))
-})
-
-test_that(".adjustment_cluster correctly removes many clusters", {
-  clust <- rep(1:6, each = 5)
-  res <- .adjustment_cluster(clust, 4)
-
-  expect_true(all(sort(unique(res)) == c(1:4)))
-})
-
-test_that(".adjustment_cluster correctly adds many clusters", {
-  clust <- rep(1:6, each = 5)
-  res <- .adjustment_cluster(clust, 9)
-
-  expect_true(all(sort(unique(res)) == c(1:9)))
-})
-
-############################
-
-## .adjustment_cluster_add is correct
-
-test_that(".adjustment_cluster_add adds", {
-  clust <- rep(1:6, each = 5)
-  res <- .adjustment_cluster_add(clust)
-
-  expect_true(all(sort(unique(res)) == c(1:7)))
-})
-
 #############################
 
 ## .average_data is correct
