002f3bf753b1b33470e3758d507e47636a51cee0
klin1234@gmail.com <klin1234@gmail.com>
2017-07-31 06:01:16 -0400

    Parallelized the functions

--- gLatentModel/R/cck.R
+++ gLatentModel/R/cck.R
@@ -1,19 +1,25 @@
-cck <- function(dat, g, translate = cor_vec, alpha = 0.05, trials = 100){
+cck <- function(dat, g, translate = cor_vec, alpha = 0.05, trials = 100,
+                cores = 2){
   n <- nrow(dat)
+  doMC::registerDoMC(cores = cores)
 
   sigma_vec <- apply(dat, 2, stats::sd)
   t0 <- g(translate(dat, sigma_vec = sigma_vec))
 
   t_boot <- rep(NA, trials)
 
-  for(i in 1:trials){
+  func <- function(i){
     e <- stats::rnorm(n)
-    t_boot[i] <- g(translate(dat, sigma_vec = sigma_vec, noise_vec = e))
+    g(translate(dat, sigma_vec = sigma_vec, noise_vec = e))
   }
+
+  i <- 1 #debugging purposes
+  t_boot <- as.numeric(foreach::"%dopar%"(foreach::foreach(i = 1:trials),
+                                          func(i)))
 
   pval <- length(which(n^(1/2)*abs(t_boot-t0) >= n^(1/2)*abs(t0)))/trials
 
-  list(pval = pval, quant = quantile(t_boot, 1-alpha), t0 = t0)
+  list(pval = pval, quant = stats::quantile(t_boot, 1-alpha), t0 = t0)
 }
 
 cor_vec <- function(dat, sigma_vec = rep(1, ncol(dat)), noise_vec = rep(1, nrow(dat))){
--- gLatentModel/R/stepdown.R
+++ gLatentModel/R/stepdown.R
@@ -1,26 +1,31 @@
-stepdown <- function(dat, g_list, translate = cor_vec, alpha = 0.05, trials = 100){
+stepdown <- function(dat, g_list, translate = cor_vec, alpha = 0.05, trials = 100,
+                     cores = 2){
   n <- nrow(dat); len <- length(g_list)
+  doMC::registerDoMC(cores = cores)
 
   sigma_vec <- apply(dat, 2, stats::sd)
   t0_vec <- sapply(1:len, function(x){g_list[[x]](translate(dat, sigma_vec = sigma_vec))})
   idx_in <- rep(TRUE, length(g_list))
 
+  func <- function(i){
+    set.seed(round*10*i)
+    e <- stats::rnorm(n)
+    t_boot_vec <- sapply(1:len, function(x){
+      if(idx_in[x]) {
+        res <- g_list[[x]](translate(dat, sigma_vec = sigma_vec, noise_vec = e))
+        n^(1/2)*abs(res - t0_vec[x])
+      } else NA})
+    if(all(is.na(t_boot_vec))) return(numeric(0)) else t_boot[i] <- max(t_boot_vec, na.rm = T)
+  }
+
   round <- 1
   while(TRUE){
-
     t_boot <- rep(NA, trials)
-    for(i in 1:trials){
-      set.seed(round*10*i)
-      e <- stats::rnorm(n)
-      t_boot_vec <- sapply(1:len, function(x){
-        if(idx_in[x]) {
-          res <- g_list[[x]](translate(dat, sigma_vec = sigma_vec, noise_vec = e))
-          n^(1/2)*abs(res - t0_vec[x])
-        } else NA})
-      if(all(is.na(t_boot_vec))) return(numeric(0)) else t_boot[i] <- max(t_boot_vec, na.rm = T)
-    }
+    i <- 1 #debugging purposes
+    t_boot <- as.numeric(foreach::"%dopar%"(foreach::foreach(i = 1:trials),
+                                            func(i)))
 
-    cutoff <- quantile(t_boot, 1-alpha, na.rm = T)
+    cutoff <- stats::quantile(t_boot, 1-alpha, na.rm = T)
     idx <- intersect(which(n^(1/2)*abs(t0_vec) >= cutoff), which(idx_in))
 
     if(length(idx) == 0) break()
